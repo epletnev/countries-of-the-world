45e29d78f997594f922bd3858aae4c1875929d1f
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-07-23 11:13:34 +0100

    Updates

---
+++ precache-manifest.4bf2b6b6b003c320e49aa9803736c021.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbccad3661373ebf8e2c0b4f78a6d58",
+    "url": "/countries-of-the-world/index.html"
+  },
+  {
+    "revision": "c918cfaf687a12c0d949",
+    "url": "/countries-of-the-world/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5d446e1cef061062a985",
+    "url": "/countries-of-the-world/static/css/main.4cf22bc7.chunk.css"
+  },
+  {
+    "revision": "c918cfaf687a12c0d949",
+    "url": "/countries-of-the-world/static/js/2.10554f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countries-of-the-world/static/js/2.10554f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d446e1cef061062a985",
+    "url": "/countries-of-the-world/static/js/main.51c95d0f.chunk.js"
+  },
+  {
+    "revision": "9cae5a43f97e21163dc0",
+    "url": "/countries-of-the-world/static/js/runtime-main.51858e16.js"
+  },
+  {
+    "revision": "f0d91b088780334cb367ea8a43e70540",
+    "url": "/countries-of-the-world/static/media/Adventure Time Logo.f0d91b08.woff"
+  }
+]);
